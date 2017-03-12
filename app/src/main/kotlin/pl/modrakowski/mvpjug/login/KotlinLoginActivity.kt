1b0addfc575958f24577d2da09d9f7d810d13eb3
Jacek Modrakowski <jacek.beny@gmail.com>
2017-03-12 19:08:04 +0100

    Adds Kotlin example.

---
+++ app/src/main/kotlin/pl/modrakowski/mvpjug/login/KotlinLoginActivity.kt
@@ -0,0 +1,11 @@
+package pl.modrakowski.mvpjug.login
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class KotlinLoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
